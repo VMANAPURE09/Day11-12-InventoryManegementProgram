3356d710afbf348c992289f562c313099192da73
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-30 18:24:01 +0530

    UC1 First Problem Added

---
+++ InventoryDataMangement/InventoryDataMangement/FetchDataForRice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataMangement
+{
+    public class FetchDataForRice
+    {
+        public Rice Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
