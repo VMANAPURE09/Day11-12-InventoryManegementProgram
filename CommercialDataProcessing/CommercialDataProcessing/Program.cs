93c07ede8ee9d7f0ed13d35a528b7f7b2ea097f9
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-05 00:37:17 +0530

    Commercial Data Processing Solution Added

---
+++ CommercialDataProcessing/CommercialDataProcessing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommercialDataProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountManagement.Data();
+        }
+    }
+}
